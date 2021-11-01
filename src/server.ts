c53e9d6b0758b8d5d813214bc8129ed82e552b66
Bruno Sanches <obrunosanches@gmail.com>
2021-11-01 18:17:25 -0300

    chore: add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello wolrd' })
+})
+
 app.listen(3333, () => console.log('Server is running'))
